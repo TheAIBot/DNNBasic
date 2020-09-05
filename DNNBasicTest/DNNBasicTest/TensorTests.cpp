6a695c5f522675d1a08577189480b224a8640f3e
TheAIBot <andreasuranus@hotmail.com>
2020-09-05 14:00:11 +0200

    rename first test file and add first test

---
+++ DNNBasicTest/DNNBasicTest/TensorTests.cpp
@@ -0,0 +1,44 @@
+#include "CppUnitTest.h"
+#include "Tensor.h"
+#include <string>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Microsoft
+{
+	namespace VisualStudio
+	{
+		namespace CppUnitTestFramework
+		{
+			template<> 
+			static std::wstring ToString<dnnbasic::tensor<float>>(const dnnbasic::tensor<float>& t)
+			{ 
+				return L"FloatTensor"; 
+			}
+			template<>
+			static std::wstring ToString<const dnnbasic::tensor<float>&>(const dnnbasic::tensor<float>& t)
+			{
+				return L"FloatTensor";
+			}
+		}
+	}
+}
+
+namespace DNNBasicTest
+{
+	TEST_CLASS(TensorTests)
+	{
+	public:
+		
+		TEST_METHOD(TensorMul)
+		{
+			dnnbasic::tensor<float> a({ 2, 1, 3 }, {1, 2, 3, 4, 5, 6});
+			dnnbasic::tensor<float> b({ 2, 1, 3 }, {3, 4, 5, 6, 7, 8});
+
+			dnnbasic::tensor<float> expected({ 2, 1, 3 }, { 3, 8, 15, 24, 35, 48 });
+			auto* actual = a * b;
+
+			Assert::AreEqual(expected, *actual);
+		}
+	};
+}
