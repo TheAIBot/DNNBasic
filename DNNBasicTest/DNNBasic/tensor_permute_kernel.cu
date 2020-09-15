02d45d27ac3ff0d1bc2acc44b8f04d387d81e382
TheAIBot <andreasuranus@hotmail.com>
2020-09-15 13:50:46 +0200

    simplyfy permute header file

--- DNNBasicTest/DNNBasic/tensor_permute_kernel.cu
+++ DNNBasicTest/DNNBasic/tensor_permute_kernel.cu
@@ -82,22 +82,23 @@ namespace dnnbasic
 	}
 
 	template <typename T>
-	void tensorPermuteInternal(const tensor<T>& input, const tensor<T>& output, const std::vector<uint32_t>& dims)
+	void tensorPermute(const tensor<T>& input, const tensor<T>& output, const std::vector<uint32_t>& dims)
 	{
 		const dim3 blockDim(256);
 		const dim3 gridDim(integerCeilDivision(input.elementCount(), blockDim.x));
 
 		cudabasic::executeKernel(permute<T>, blockDim, gridDim, input.getGPUArrayConst(), output.getGPUArray(), tensorDims<T>(input), tensorDims<T>(output), permuteIndicies(dims));
 	}
-	void tensorPermute(const tensor<bool>& input, const tensor<bool>& output, const std::vector<uint32_t>& dims) { tensorPermuteInternal(input, output, dims); }
-	void tensorPermute(const tensor<uint8_t>& input, const tensor<uint8_t>& output, const std::vector<uint32_t>& dims) { tensorPermuteInternal(input, output, dims); }
-	void tensorPermute(const tensor<uint16_t>& input, const tensor<uint16_t>& output, const std::vector<uint32_t>& dims) { tensorPermuteInternal(input, output, dims); }
-	void tensorPermute(const tensor<uint32_t>& input, const tensor<uint32_t>& output, const std::vector<uint32_t>& dims) { tensorPermuteInternal(input, output, dims); }
-	void tensorPermute(const tensor<uint64_t>& input, const tensor<uint64_t>& output, const std::vector<uint32_t>& dims) { tensorPermuteInternal(input, output, dims); }
-	void tensorPermute(const tensor<int8_t>& input, const tensor<int8_t>& output, const std::vector<uint32_t>& dims) { tensorPermuteInternal(input, output, dims); }
-	void tensorPermute(const tensor<int16_t>& input, const tensor<int16_t>& output, const std::vector<uint32_t>& dims) { tensorPermuteInternal(input, output, dims); }
-	void tensorPermute(const tensor<int32_t>& input, const tensor<int32_t>& output, const std::vector<uint32_t>& dims) { tensorPermuteInternal(input, output, dims); }
-	void tensorPermute(const tensor<int64_t>& input, const tensor<int64_t>& output, const std::vector<uint32_t>& dims) { tensorPermuteInternal(input, output, dims); }
-	void tensorPermute(const tensor<float>& input, const tensor<float>& output, const std::vector<uint32_t>& dims) { tensorPermuteInternal(input, output, dims); }
-	void tensorPermute(const tensor<double>& input, const tensor<double>& output, const std::vector<uint32_t>& dims) { tensorPermuteInternal(input, output, dims); }
+
+	template void tensorPermute(const tensor<bool>& input, const tensor<bool>& output, const std::vector<uint32_t>& dims);
+	template void tensorPermute(const tensor<uint8_t>& input, const tensor<uint8_t>& output, const std::vector<uint32_t>& dims);
+	template void tensorPermute(const tensor<uint16_t>& input, const tensor<uint16_t>& output, const std::vector<uint32_t>& dims);
+	template void tensorPermute(const tensor<uint32_t>& input, const tensor<uint32_t>& output, const std::vector<uint32_t>& dims);
+	template void tensorPermute(const tensor<uint64_t>& input, const tensor<uint64_t>& output, const std::vector<uint32_t>& dims);
+	template void tensorPermute(const tensor<int8_t>& input, const tensor<int8_t>& output, const std::vector<uint32_t>& dims);
+	template void tensorPermute(const tensor<int16_t>& input, const tensor<int16_t>& output, const std::vector<uint32_t>& dims);
+	template void tensorPermute(const tensor<int32_t>& input, const tensor<int32_t>& output, const std::vector<uint32_t>& dims);
+	template void tensorPermute(const tensor<int64_t>& input, const tensor<int64_t>& output, const std::vector<uint32_t>& dims);
+	template void tensorPermute(const tensor<float>& input, const tensor<float>& output, const std::vector<uint32_t>& dims);
+	template void tensorPermute(const tensor<double>& input, const tensor<double>& output, const std::vector<uint32_t>& dims);
 }
