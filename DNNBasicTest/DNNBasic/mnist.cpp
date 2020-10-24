b76c15ddf0682e9819d501fe44fd99520d3fb0fd
TheAIBot <andreasuranus@hotmail.com>
2020-10-24 05:53:42 +0200

    add default names to dimensions that can be overidden

--- DNNBasicTest/DNNBasic/mnist.cpp
+++ DNNBasicTest/DNNBasic/mnist.cpp
@@ -13,7 +13,20 @@ namespace dnnbasic::datasets::mnist
 	static constexpr uint32_t trainSamplesCount = 60'000;
 	static constexpr uint32_t testSamplesCount = 10'000;
 
-	supervisedDataset<uint8_t> loadSet(const std::string& folderPath, const std::string& inputFileName, const std::string& outputFileName, const uint32_t sampleCount, const uint32_t batchSize)
+	static constexpr uint32_t imageWidth = 28;
+	static constexpr uint32_t imageHeight = 28;
+	static constexpr uint32_t labelsCount = 10;
+
+	static supervisedDataset<uint8_t> loadSet(
+		const std::string& folderPath, 
+		const std::string& inputFileName, 
+		const std::string& outputFileName, 
+		const uint32_t sampleCount, 
+		const uint32_t batchSize,
+		const std::string& batchName,
+		const std::string& heightName,
+		const std::string& widthName,
+		const std::string& labelName)
 	{
 		dataloader input(folderPath + "/" + inputFileName);
 		dataloader output(folderPath + "/" + outputFileName);
@@ -28,18 +41,18 @@ namespace dnnbasic::datasets::mnist
 		const uint32_t batches = sampleCount / batchSize;
 		for (size_t i = 0; i < batches; i++)
 		{
-			auto inputCharData = input.loadNext(batchSize * 28 * 28);
+			auto inputCharData = input.loadNext(batchSize * imageWidth * imageHeight);
 			auto outputCharData = input.loadNext(batchSize);
 
 			std::vector<uint8_t> inputData(inputCharData.begin(), inputCharData.end());
-			std::vector<uint8_t> outputData((size_t)batchSize * 10);
+			std::vector<uint8_t> outputData((size_t)batchSize * labelsCount);
 			for (size_t i = 0; i < batchSize; i++)
 			{
-				outputData[i * 10 + outputCharData[i]] = 1;
+				outputData[i * labelsCount + outputCharData[i]] = 1;
 			}
 
-			tensor<uint8_t> input({ batchSize, 1, 28, 28 }, inputData);
-			tensor<uint8_t> output({ batchSize, 10 }, outputData);
+			tensor<uint8_t> input({ batchSize, imageHeight, imageWidth }, { batchName, heightName, widthName }, inputData);
+			tensor<uint8_t> output({ batchSize, labelsCount }, { batchName, labelName }, outputData);
 
 			inputs.push_back(input);
 			outputs.push_back(output);
@@ -48,13 +61,25 @@ namespace dnnbasic::datasets::mnist
 		return supervisedDataset<uint8_t>(inputs, outputs);
 	}
 
-	supervisedDataset<uint8_t> loadTrainingSet(const std::string& folderPath, const uint32_t batchSize)
+	supervisedDataset<uint8_t> loadTrainingSet(
+		const std::string& folderPath, 
+		const uint32_t batchSize,
+		const std::string& batchName,
+		const std::string& heightName,
+		const std::string& widthName,
+		const std::string& labelName)
 	{
-		return loadSet(folderPath, trainInputFile, trainOutputFile, trainSamplesCount, batchSize);
+		return loadSet(folderPath, trainInputFile, trainOutputFile, trainSamplesCount, batchSize, batchName, heightName, widthName, labelName);
 	}
 
-	supervisedDataset<uint8_t> loadTestSet(const std::string& folderPath, const uint32_t batchSize)
+	supervisedDataset<uint8_t> loadTestSet(
+		const std::string& folderPath, 
+		const uint32_t batchSize,
+		const std::string& batchName,
+		const std::string& heightName,
+		const std::string& widthName,
+		const std::string& labelName)
 	{
-		return loadSet(folderPath, testInputFile, testOutputFile, testSamplesCount, batchSize);
+		return loadSet(folderPath, testInputFile, testOutputFile, testSamplesCount, batchSize, batchName, heightName, widthName, labelName);
 	}
 }
--- DNNBasicTest/DNNBasic/mnist.h
+++ DNNBasicTest/DNNBasic/mnist.h
@@ -11,6 +11,18 @@
 
 namespace dnnbasic::datasets::mnist
 {
-	supervisedDataset<uint8_t> loadTrainingSet(const std::string& folderPath, const uint32_t batchSize);
-	supervisedDataset<uint8_t> loadTestSet(const std::string& folderPath, const uint32_t batchSize);
+	supervisedDataset<uint8_t> loadTrainingSet(
+		const std::string& folderPath, 
+		const uint32_t batchSize,
+		const std::string& batchName = "batch",
+		const std::string& heightName = "height",
+		const std::string& widthName = "width",
+		const std::string& labelName = "label");
+	supervisedDataset<uint8_t> loadTestSet(
+		const std::string& folderPath, 
+		const uint32_t batchSize,
+		const std::string& batchName = "batch",
+		const std::string& heightName = "height",
+		const std::string& widthName = "width",
+		const std::string& labelName = "label");
 }
