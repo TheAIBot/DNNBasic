ee12e08775c5b7596a789485d6f4ba991eb7dac3
Captaine <peter_ebert@live.dk>
2020-11-01 15:11:11 +0100

    loss data in own file

---
+++ DNNBasicTest/DNNBasic/loss_data.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <memory>
+#include <cstdint>
+#include <functional>
+#include "tensor.h"
+#include "tensor_node.h"
+
+namespace dnnbasic::loss
+{
+	template<typename T>
+	class lossData
+	{
+	private:
+		tensor<T> gradient;
+		std::shared_ptr<tensorNode<T>> leafNode;
+		tensor<T> errorTensor;
+		std::function<T(tensor<T>)> errorCalcMethod;
+
+	public:
+		lossData(tensor<T> gradient, tensor<T> error, std::shared_ptr<tensorNode<T>> leafNode, const std::function<T(tensor<T>)>& errorCalcMethod);
+
+		void backward(optimizer::optimizer* opti);
+
+		T getError();
+	};
+}
